a282b8e049d8300085389a29ec7e8fb61065f4f2
Giles Booth <giles.booth@gmail.com>
2021-10-24 22:34:26 +0100

    add my own tweaks
    Bush House location, objects, ability to pick up and move some things, inventory, shouting

--- simplemud.py
+++ simplemud.py
@@ -19,6 +19,9 @@ Some ideas for things to try adding:
     * A shop from which to buy items
 
 author: Mark Frimston - mfrimston@gmail.com
+
+modified by Giles Booth to add Bush House location, shouting, objects,
+ability to pick up and drop some objects, inventory
 """
 
 import time
@@ -26,17 +29,77 @@ import time
 # import the MUD server class
 from mudserver import MudServer
 
+splash = '''
+ ______                  
+/_  __/__ _    _____ ____
+ / / / _ \ |/|/ / -_) __/
+/_/  \___/__,__/\__/_/   
+           ___           
+     ___  / _/           
+    / _ \/ _/            
+   _\___/_/  __       __ 
+  / _ )___ _/ /  ___ / / 
+ / _  / _ `/ _ \/ -_) /  
+/____/\_,_/_.__/\__/_/   
+                         '''
+
 
 # structure defining the rooms in the game. Try adding more rooms to the game!
 rooms = {
-    "Tavern": {
-        "description": "You're in a cozy tavern warmed by an open fire.",
-        "exits": {"outside": "Outside"},
+    "Studio Managers\' Common Room": {
+        "description": "You're in a small dimly-lit room.\n",
+        "exits": {"outside": "Corridor"},
     },
-    "Outside": {
-        "description": "You're standing outside a tavern. It's raining.",
-        "exits": {"inside": "Tavern"},
+    "Corridor": {
+        "description": "You're standing in a corridor outside the Studio Managers\' Common Room.\nIt leads off to the north.",
+        "exits": {"inside": "Studio Managers\' Common Room", "north": "Duty Operations Manager\'s office"},
+    },
+    "Duty Operations Manager\'s office": {
+        "description": "The Duty Operation Manager\'s office. The DOM is on the phone.",
+        "exits": {"south": "Corridor", "north": "Landing"},
+    },
+    "Landing": {
+        "description": "A beautiful landing. The walls and floors are covered in marble.",
+        "exits": {"south": "Duty Operations Manager\'s office", "down": "Club"},
+    },
+    "Club": {
+        "description": "The Bush House BBC Club. \nIf you\'d like to play a proper adventure set in Bush House visit http://suppertime.co.uk/tower-of-babel/play.html",
+        "exits": {"up": "Landing"},
     }
+}
+
+objects = {
+    "whiteboard" : {
+    	"description": "A dirty whiteboard and a marker pen attached by a piece of red leader tape.",
+    	"portable": False,
+    	"location": "Studio Managers\' Common Room"
+    },
+    "sofa" : {
+    	"description": "An old saggy sofa covered in dubious stains.",
+    	"portable": False,
+    	"location": "Studio Managers\' Common Room"
+    },
+    "key" : {
+    	"description": "A key with the words STUDIO stamped on it.",
+    	"portable": True,
+    	"location": "Duty Operations Manager\'s office"
+    },
+    "fishtank" : {
+    	"description": "A long fishtank in which bored tropical fish are swimming",
+    	"portable": False,
+    	"location": "Club"
+    },
+    "fruit machine" : {
+    	"description": "A fruit machine with flashing lights.",
+    	"portable": False,
+    	"location": "Club"
+    },
+    "bottle of beer" : {
+    	"description": "A bottle of Young\'s Ram Rod.",
+    	"portable": True,
+    	"location": "Club"
+    },
+
 }
 
 # stores the players in the game
@@ -67,6 +130,7 @@ while True:
         players[id] = {
             "name": None,
             "room": None,
+            "inventory": [],
         }
 
         # send the new player a prompt for their name
@@ -84,7 +148,7 @@ while True:
         for pid, pl in players.items():
             # send each player a message to tell them about the diconnected
             # player
-            mud.send_message(pid, "{} quit the game".format(
+            mud.send_message(pid, "{} has left the building".format(
                                                         players[id]["name"]))
 
         # remove the player's entry in the player dictionary
@@ -103,7 +167,7 @@ while True:
         if players[id]["name"] is None:
 
             players[id]["name"] = command
-            players[id]["room"] = "Tavern"
+            players[id]["room"] = "Studio Managers\' Common Room"
 
             # go through all the players in the game
             for pid, pl in players.items():
@@ -112,9 +176,9 @@ while True:
                                                         players[id]["name"]))
 
             # send the new player a welcome message
-            mud.send_message(id, "Welcome to the game, {}. ".format(
+            mud.send_message(id, splash+"\nWelcome to the Tower of Babel, {}.\nThis is a test game made in MUD Pi with just a few rooms but you can talk to other players. ".format(
                                                            players[id]["name"])
-                             + "Type 'help' for a list of commands. Have fun!")
+                             + "\n\nType 'help' for a list of commands. Have fun!\n")
 
             # send the new player the description of their current room
             mud.send_message(id, rooms[players[id]["room"]]["description"])
@@ -127,11 +191,20 @@ while True:
 
             # send the player back the list of possible commands
             mud.send_message(id, "Commands:")
-            mud.send_message(id, "  say <message>  - Says something out loud, "
+            mud.send_message(id, "  say <message>   - Say something out loud, "
                                  + "e.g. 'say Hello'")
-            mud.send_message(id, "  look           - Examines the "
+            mud.send_message(id, "  shout <message> - Shout something, "
+                                 + "e.g. 'shout Hello'")
+            mud.send_message(id, "  look            - Look at the "
                                  + "surroundings, e.g. 'look'")
-            mud.send_message(id, "  go <exit>      - Moves through the exit "
+            mud.send_message(id, "  examine <thing> - Examine an object in "
+                                 + "detail, e.g. 'examine key'")
+            mud.send_message(id, "  take <thing>    - Pick an object up "
+                                 + "e.g. 'take key'")
+            mud.send_message(id, "  drop <thing>    - Put an object down "
+                                 + "e.g. 'drop key'")
+            mud.send_message(id, "  inventory       - List what you\'re carrying")
+            mud.send_message(id, "  go <exit>       - Move through the exit "
                                  + "specified, e.g. 'go outside'")
 
         # 'say' command
@@ -145,6 +218,24 @@ while True:
                     mud.send_message(pid, "{} says: {}".format(
                                                 players[id]["name"], params))
 
+        # 'shout' command
+        elif command == "shout":
+
+            # go through every player in the game
+            for pid, pl in players.items():
+                # send them a message telling them what the player said
+                mud.send_message(pid, "{} shouts: {}".format(
+                                            players[id]["name"], params))
+
+
+        # examine object if it exists and is in your room
+        elif command == "examine":
+            if params in objects and objects[params]["location"] == players[id]["room"]:
+                mud.send_message(pid, objects[params]["description"])
+            else:
+                mud.send_message(pid, "There\'s no {} here.".format(params))
+			
+					
         # 'look' command
         elif command == "look":
 
@@ -163,6 +254,14 @@ while True:
                     if players[pid]["name"] is not None:
                         # add their name to the list
                         playershere.append(players[pid]["name"])
+                        
+            # list objects in your location
+            objectshere = []
+            for thing in objects:
+            	if objects[thing]["location"] == players[pid]["room"]:
+            		objectshere.append("a " + thing)
+            if len(objectshere) > 0:
+	            mud.send_message(id, "You can see {}".format(", ".join(objectshere)))
 
             # send player a message containing the list of players in the room
             mud.send_message(id, "Players here: {}".format(
@@ -171,6 +270,38 @@ while True:
             # send player a message containing the list of exits from this room
             mud.send_message(id, "Exits are: {}".format(
                                                     ", ".join(rm["exits"])))
+
+        elif command == "take":
+            if params in objects and objects[params]["location"] == players[id]["room"]:
+                if objects[params]["portable"]:
+                    mud.send_message(id, "You take the {}.".format(params))
+                    players[id]["inventory"].append(params)
+                    objects[params]["location"] = players[id]
+                else:
+                    mud.send_message(id, "The {} is far too heavy to pick up.".format(params))
+            else:
+                mud.send_message(id, "There\'s no {} here.".format(params))
+                
+        elif command == "drop":
+            if params in players[id]["inventory"]:
+                mud.send_message(id, "You drop the {}.".format(params))
+                # remove object from player's inventory
+                players[id]["inventory"].remove(params)
+                # change object's location to current room
+                objects[params]["location"] = players[id]["room"]
+            else:
+                mud.send_message(id, "You\'re not carrying a {}.".format(params))
+
+                
+        elif command == "inventory":
+            if len(players[id]["inventory"]) > 0:
+                inv_list = "You\'re carrying:"
+                for item in players[id]["inventory"]:
+                    inv_list = inv_list + "\n- a " + item
+                mud.send_message(id, inv_list)
+            else:
+                mud.send_message(id, "You\'re not carrying anything.")
+            
 
         # 'go' command
         elif command == "go":
@@ -218,9 +349,16 @@ while True:
             # the specified exit wasn't found in the current room
             else:
                 # send back an 'unknown exit' message
-                mud.send_message(id, "Unknown exit '{}'".format(ex))
+                mud.send_message(id, "You can\'t go '{}'".format(ex))
 
+		# player pressed enter with no command
+        elif command == '':
+        	mud.send_message(id, "Give me a command.")
+
+        elif command == 'quit':
+        	mud.send_message(id, "To leave the game press ctrl-] and type 'quit' again at the telnet prompt.")
+        
         # some other, unrecognised command
         else:
             # send back an 'unknown command' message
-            mud.send_message(id, "Unknown command '{}'".format(command))
+            mud.send_message(id, "I don\'t understand '{}'".format(command))
